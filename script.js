c6a8c59cc2a01dd4571e49a183db470d987c5e20
rockburguersbnu <benhurbnu2@gmail.com>
2025-06-29 20:09:13 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Rolagem suave
-document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click', e=>{
-  e.preventDefault();
-  document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-}));
